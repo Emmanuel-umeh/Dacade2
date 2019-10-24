deb6f4847a7469d663451c7b2f41d73e13a5a0bc
Emmanuel <Emmanuelsumeh@gmail.com>
2019-10-24 02:52:36 -0700

    index.html

---
+++ assets/js/like.js
@@ -0,0 +1,9 @@
+(function (d, e, s) {
+    if (d.getElementById("likebtn_wjs")) return;
+    a = d.createElement(e);
+    m = d.getElementsByTagName(e)[0];
+    a.async = 1;
+    a.id = "likebtn_wjs";
+    a.src = s;
+    m.parentNode.insertBefore(a, m)
+})(document, "script", "//w.likebtn.com/js/w/widget.js");
