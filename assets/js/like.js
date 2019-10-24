7bb34ada6c09a37001fe8d78b171b6b843aae931
Emmanuel <Emmanuelsumeh@gmail.com>
2019-10-24 05:02:05 -0700

    lifehack

--- assets/js/like.js
+++ assets/js/like.js
@@ -1,5 +1,5 @@
 $(function(){
-	$(document).one('click', '.like-review', function(e) {
+	$(document).on('click', '.like-review', function(e) {
 		$(this).html('<i class="fa fa-heart" aria-hidden="true"></i> You liked this');
 		$(this).children('.fa-heart').addClass('animate-like');
 	});
